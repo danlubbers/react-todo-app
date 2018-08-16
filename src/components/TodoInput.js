b69f38932e5a14f5e834d1c1f0f2124a01ef4d7b
Dan Lubbers <lubbers.dan@gmail.com>
2018-08-16 14:28:30 -0400

    finished with tutorial

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,32 +1,33 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './TodoInput.css';
 
-export default class TodoInput extends Component {
-    constructor(props) {
-        super(props)
+export default class TodoInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: this.props.todoText};
 
-        this.state = {
-            value: ''
-        };
+    this.handleChange = this.handleChange.bind(this);
+    this.addTodo = this.addTodo.bind(this);
+  }
 
-        this.handleChange = this.handleChange.bind(this);
-        this.addTodo = this.addTodo.bind(this);
+  handleChange(e) {
+    this.setState({value: e.target.value});
+  }
+
+  addTodo(todo) {
+    // Ensure a todo was actually entered before submitting
+    if (todo.length > 0) {
+      this.props.addTodo(todo);
+      this.setState({value: ''});
     }
+  }
 
-    handleChange() {
-        this.setstate({});
-    }
-
-    addTodo(todo) {
-        this.setState({});
-    }
-
-    render() {
-        return (
-            <div>
-                <input type="text" value="" onChange={this.handleChange} />
-                <button className="btn btn=primary" onClick={_=> (this.state.value)}>Submit</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        <button className="btn btn-primary" onClick={() => this.addTodo(this.state.value)}>Submit</button>
+      </div>
+    );
+  }
 }
